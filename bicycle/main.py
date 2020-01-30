e411a0e6c0333f7052f8c5e4f721599f93ab4306
Matt Frankmore <m.frankmore@outlook.com>
2020-01-30 13:47:57 -0700

    Added main.py

---
+++ bicycle/main.py
@@ -0,0 +1,10 @@
+from wheel import Wheel
+
+print(f"burst pressure = {Wheel.BURST_PRESSURE}")
+
+wheel = Wheel(diameter = 13.0, pressure = 24.0)
+
+print(f"pressure = {wheel.pressure}") # getter for a property
+wheel.pressure = 10000.0 # setter notation
+print(f"pressure = {wheel.pressure}") # getter for a property
+print(f"burst = {wheel.burst}") # getter for a property
