9b15fd5e074c46cbf11523b13c33721ddcdc0fbf
Matt Frankmore <m.frankmore@outlook.com>
2020-02-24 23:58:13 -0700

    Finished Classroom.py

--- assign1/classroom.py
+++ assign1/classroom.py
@@ -2,9 +2,11 @@ from typing import Sequence
 from student import Student
 
 class Classroom:
-    def __init__(self, subject : str, capacity : int):
+    def __init__(self, subject : str, capacity : int, attendance : Student, pupil : str = Student.name, grade : int = Student.DEFAULT_GRADE):
         self._subject : str = subject
         self._capacity : int = capacity
+        self._attendence : Sequence[Student] = (Student(pupil, grade), Student(pupil, grade), Student(pupil, grade))  
+
 
     
     @property
@@ -14,3 +16,15 @@ class Classroom:
     @property
     def capacity(self):
         return self._capacity
+
+    @property
+    def pupil1(self):
+        return self._attendence[0]
+
+    @property
+    def pupil2(self):
+        return self._attendence[1]
+
+    @property
+    def pupil3(self):
+        return self._attendence[2]
