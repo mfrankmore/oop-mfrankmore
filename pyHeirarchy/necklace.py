b2e28c5863baf5aefe684ac1ac079b2987172d66
Matt Frankmore <m.frankmore@outlook.com>
2020-04-10 11:29:44 -0600

    Finished necklace.py

--- pyHeirarchy/necklace.py
+++ pyHeirarchy/necklace.py
@@ -1,10 +1,28 @@
 from jewelry import Jewelry
 
 class Necklace(Jewelry):
-    def __init__(self, metal : str, gem : str):
+    DEFAULT_METAL : str = "gold"
+    DEFAULT_GEM : str = "diamond"
+
+    def __init__(self, metal : str = DEFAULT_METAL, gem : str = DEFAULT_GEM):
         super(Necklace,self).__init__(polished = True)
         self._metal = metal
         self._gem = gem
 
     @property
-    def 
+    def metal(self) -> str:
+        return self._metal
+
+    @metal.setter
+    def metal(self, value : str) -> None:
+        self._metal = value
+
+    @property
+    def gem(self) -> str:
+        return self._gem
+
+    @gem.setter
+    def gem(self, value : str) -> None:
+        self._gem = value
+
+    
